2c15cd9ed8d6f7b613284dac2a4c012ff4930726
S4R4GDL <saragdl7321@gmail.com>
2025-08-19 11:00:58 -0300

    FIX - Ajustando propriedades e injeção de email, puxando do properties as variáveis de email

--- src/main/java/br/ueg/tc/pipa_email/PipaEmailApplication.java
+++ src/main/java/br/ueg/tc/pipa_email/PipaEmailApplication.java
@@ -1,6 +1,5 @@
 package br.ueg.tc.pipa_email;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "br.ueg.tc")
